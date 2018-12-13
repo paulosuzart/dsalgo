27e9382920a15e48af169476129a9d548b3290a1
Paulo Suzart <paulosuzart@gmail.com>
2018-12-14 00:51:18 +0100

    Fixed docs

--- src/main/java/skiena/three/SimpleBinTree.java
+++ src/main/java/skiena/three/SimpleBinTree.java
@@ -7,15 +7,32 @@ import java.util.Optional;
 /**
  * 3-8. Design a data structure to support the following operations:
  *
- * insert(x,T) -- Insert item x into the set T. delete(k,T) -- Delete the kth smallest element from T. member(x,T) -- Return true iff x∈T.
- * All operations must take O(logn) time on an n-element set.
+ * insert(x,T) -- Insert item x into the set T.
+ *
+ * delete(k,T) -- Delete the kth smallest element from T.
+ *
+ * member(x,T) -- Return true iff x∈T.
+ *
+ * All operations must take O(log n) time on an n-element set.
  */
 public class SimpleBinTree<T extends Comparable<T>> {
 
   Node<T> root;
 
+  int size;
+
+  Node<T> min;
+  Node<T> max;
+
+  /**
+   * Successor users depth first algo to look for a possible successor of {@code item}.
+   *
+   * It's possible that there's no successor on the tree for the givem {@code item}.
+   *
+   * @param item to look for successor
+   * @return {@code Node<T>} as successor or null if none is found.
+   */
   Node<T> successor(T item) {
-    Node search = root;
     Deque<Node<T>> toCompare = new LinkedList<>();
     toCompare.addFirst(root);
 
