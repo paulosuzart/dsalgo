58ad1a7a490f43a069826a85f2c99a007cd1695f
Paulo Suzart <paulosuzart@gmail.com>
2018-12-22 08:36:02 +0100

    add more explanation

--- src/main/java/udemy/ReverseInt.java
+++ src/main/java/udemy/ReverseInt.java
@@ -4,6 +4,13 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.stream.Collectors;
 
+/**
+ * Given an int, reverse it as in:
+ *
+ * reverse(90) == 9
+ * reverse(-51) == -15
+ * reverse(18) == 81
+ */
 public class ReverseInt {
 
   int reverse(int n) {
@@ -17,10 +24,14 @@ public class ReverseInt {
       n = -1 * n;
     }
 
+    // uses string as list to reverse using Collections.reverse
     var reversedString = Arrays.asList(String.valueOf(n).split(""));
     Collections.reverse(reversedString);
 
+    // turn list of strings back to single string then parses as int.
     var reversed = Integer.parseInt(reversedString.stream().collect(Collectors.joining()));
+
+    // put value back to negative again, if needed.
     return multiplyFactor * reversed;
 
   }
